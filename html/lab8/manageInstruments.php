23af79f12004092713ea0eab980c22131089e38a
johnson subedi <subede.johnson@gmail.com>
2023-11-09 21:01:00 +0000

    Lab9 updates

--- html/lab8/manageInstruments.php
+++ html/lab8/manageInstruments.php
@@ -3,6 +3,46 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$dark = "dark"; 
+$mode = isset($_COOKIE['user_mode']) ? $_COOKIE['user_mode'] : "light";
+$button_pressed = "button_pressed";
+$button_label = "Toggle Dark/Light Modes";
+
+if (!isset($_COOKIE['user_mode'])) {
+    setcookie('user_mode', $mode, time() + 60 * 60 * 24 * 365, '/');
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit();
+}
+
+$button_pressed = "button_pressed";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST[$button_pressed])) {
+    $new_mode = $_COOKIE['user_mode'] === 'light' ? 'dark' : 'light';
+    setcookie('user_mode', $new_mode, time() + 60 * 60 * 24 * 365, '/');
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit();
+}
+
+session_start();
+$dark = "dark";
+
+if (!isset($_SESSION['num_deleted'])) {
+    $_SESSION['num_deleted'] = 0;
+}
+
+if (isset($_POST['logout'])) {
+    session_unset();
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit();
+}
+
+if (isset($_POST['username'])) {
+    $_SESSION['username'] = $_POST['username'];
+    header("Location: {$_SERVER['REQUEST_URI']}", true, 303);
+    exit();
+}
+
+
 // Function to establish a database connection
 function getDatabaseConnection() {
     $config = parse_ini_file('/home/johnsonsubedi/CSC362/mysqli.ini');
@@ -22,14 +62,49 @@ function getDatabaseConnection() {
 }
 
 // Function to generate an HTML table from MySQL result
-function resultToHtmlTable($result) {
+function resultToHtmlTable($result, $mode) {
     $resultBody = $result->fetch_all();
     $numRows = $result->num_rows;
     $numCols = $result->field_count;
     $fields = $result->fetch_fields();
     ?>
+<?php
+$mode = isset($_COOKIE['user_mode']) ? $_COOKIE['user_mode'] : "light";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Document</title> -->
 
+    <?php
+    if ($_COOKIE['user_mode'] == "light") {
+    ?>
+        <link rel="stylesheet" href="./basic.css">
+    <?php
+    } else {
+    ?>
+        <link rel="stylesheet" href="./darkmode.css">
+    <?php
+    } 
+    ?>
+</head>
+<body>
+    
+    <a href="./">Back to Home Page</a> &nbsp;
     <p>This table has <?php echo $numRows; ?> rows and <?php echo $numCols; ?> columns.</p>
+    <?php
+    $dark = 'dark';
+    ?>
+    <h1>
+        <?php echo $_COOKIE['user_mode'] == $dark? "Welcome to the Dark Mode" : "Delete Instruments";?>
+    </h1>
+
+    <!-- Toggle Button -->
+    <form method=POST>
+    <input type="submit" name="<?= $button_pressed ?>" value="Toggle Light/Dark Mode"/>
+    </form>
 
     <form action="deleteFromTable.php" method="POST">
         <table>
@@ -64,9 +139,10 @@ function resultToHtmlTable($result) {
     <form method="POST" action="deleteall.php">
         <input type="submit", name="delete_all", value="Delete All Records">
     </form>
+</body>
+</html>
 
-    <?php
-
+<?php
 }
 
 // Main code
@@ -80,12 +156,10 @@ if ($result === false) {
 }
 
 if ($result->num_rows > 0) {
-    resultToHtmlTable($result);
+    resultToHtmlTable($result, $_COOKIE[$mode] ?? "light");
 } else {
     echo "No records found";
-    resultToHtmlTable($result);
+    resultToHtmlTable($result, $_COOKIE[$mode] ?? "light");
 }
 
 ?>
-
-
